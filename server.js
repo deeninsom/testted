a9648400f5bb7b400eac4c0c84b3dfdcb7c590ff
ARIS NC <ariezncahyo@gmail.com>
2024-12-05 18:32:57 +0700

    on close candle

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const SynchronizationListener = require("metaapi.cloud-sdk").SynchronizationListener;
+const SynchronizationListener =
+  require("metaapi.cloud-sdk").SynchronizationListener;
 const MetaApi = require("metaapi.cloud-sdk").default;
 const dayjs = require("dayjs");
 
@@ -6,19 +7,30 @@ const token = process.env.TOKEN;
 const accountId = process.env.ACCOUNT_ID;
 const symbol = process.env.SYMBOL;
 const domain = process.env.DOMAIN;
+const timeframe = "1m";
 
 const api = new MetaApi(token, { domain });
 
-let prevTime = dayjs().add(1, "minute").format("mm");
-
 class QuoteListener extends SynchronizationListener {
+  constructor() {
+    super();
+    this.time = dayjs()
+      .add(1, "minute")
+      .set("second", 0)
+      .set("millisecond", 0)
+      .toISOString();
+  }
   async onCandlesUpdated(_instanceIndex, candles) {
     for (const candle of candles) {
-      if (candle.symbol === symbol) {
-        const newBar = dayjs(candle.time).format("mm") > prevTime;
-        if (newBar) {
-          console.log("new bar", candle)
-          prevTime = dayjs().format("mm");
+      console.log(candle.symbol, candle.timeframe);
+      if (candle.symbol === symbol && candle.timeframe === timeframe) {
+        const timenow = dayjs(candle.time)
+          .set("second", 0)
+          .set("millisecond", 0)
+          .toISOString();
+        if (timenow > this.time) {
+          console.log("new candle=", timenow, "Close=", candle.close);
+          this.time = timenow;
         }
       }
     }
@@ -33,8 +45,10 @@ async function main() {
     connection.addSynchronizationListener(quoteListener);
     await connection.connect();
     await connection.waitSynchronized();
-    await connection.subscribeToMarketData(symbol, [{ type: "candles", timeframe: "1m", intervalInMilliseconds: 60000 }]);
-  }catch(error) {
+    await connection.subscribeToMarketData(symbol, [
+      { type: "candles", timeframe, intervalInMilliseconds: 10000 },
+    ]);
+  } catch (error) {
     console.log(error);
   }
 }
